296526068f935c39052f859ce70cf36c3f8556f4
Shawn Oakley <shawn.oakley@youvisit.com>
2019-01-21 16:12:25 +0100

    Adds layout plugin with injectable layout

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -2,16 +2,14 @@ import React from "react";
 import { Link } from "gatsby";
 import Header from './../components/header';
 import ThreeContainer from './../components/threeContainer';
-import Layout from "../components/layout"
 import SCENE_CONSTANTS from './../constants/scenes';
 
-
+// https://divdev.io/animating-gatsby-pt/
 
 // Intro point for gatsby app
 export default () => (
-    <Layout>
-      <Link to="/contact/">Contact</Link> 
-      <Header headerText="Hello Gatsby!" />
+    <div>
+      <Header />
       <ThreeContainer sceneName={SCENE_CONSTANTS.SCENE_INTRO}/>
-    </Layout>
+    </div>
   )
--- hello-world/src/pages/projects.js
+++ hello-world/src/pages/projects.js
@@ -1,6 +1,5 @@
 import React from "react"
 import EventEmitter from "EventEmitter";
-import Layout from "../components/layout";
 import EventManager from "../components/eventManager";
 import ThreeContainer from './../components/threeContainer';
 import SCENE_CONSTANTS from './../constants/scenes';
@@ -8,9 +7,7 @@ import SCENE_CONSTANTS from './../constants/scenes';
 let EventBus = new EventEmitter();
 
 export default () => (
-  <Layout>
     <EventManager eventBus={EventBus}>
       <ThreeContainer sceneName={SCENE_CONSTANTS.SCENE_PROJECTS} eventBus={EventBus} />
     </EventManager>
-  </Layout>
 )
