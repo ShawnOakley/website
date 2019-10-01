d0ba46cd561d9d30faf0f4050445042ab6c82896
shawn.oakley <shawn.oakley@youvisit.com>
2019-10-01 18:09:31 -0400

    Adds seo component

--- hello-world/gatsby-config.js
+++ hello-world/gatsby-config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  siteMetadata: {
+    title: "Shawn Oakley",
+    titleTemplate: "%s · Fullstack Developer and UX Designer",
+    description:
+      "Full Stack Developer with years of strong experience in both Front End and Back End Development and a passion for collaboration and mastering new technologies.",
+    url: "https://www.shawnoakley.com", // No trailing slash allowed!
+    // image: "/images/snape.jpg", // Path to your image you placed in the 'static' folder
+    // twitterUsername: "@occlumency",
+  },  
   plugins: [
     'gatsby-plugin-emotion',
     'gatsby-plugin-layout',
