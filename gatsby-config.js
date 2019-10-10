572421432110547615712ee12c35bf842dcc5753
shawn.oakley <shawn.oakley@youvisit.com>
2019-10-10 00:38:10 -0400

    Adds manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,24 @@ module.exports = {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
         trackingId: "UA-45266681-3",
       },
-    },    
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Shawn Oakley - Full Stack Web Developer and UX Designer",
+        short_name: "Shawn Oakley",
+        start_url: "/",
+        background_color: "#88FFFF",
+        theme_color: "#88FFFF",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },        
   ],
 }
